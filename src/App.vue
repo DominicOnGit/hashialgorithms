9c3da35d266202994364b8130f3ca151b7180439
Dominic Jost <Dominic.Jost@gmail.com>
2025-07-13 18:51:50 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,27 @@
 
 <template>
   <header>
-    <h1>Hashi Algorithm</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand">Hashi Algorithm</span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <RouterLink class="nav-link" exactActiveClass="active" to="/">Levels</RouterLink>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
   <main>
     <RouterView />
